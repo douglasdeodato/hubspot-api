4d242a0835cdf7969ce08737804c34a6b37b390c
douglas <douglasdeodato@gmail.com>
2023-08-24 14:39:53 +0100

    refactory

--- single-contact.py
+++ single-contact.py
@@ -19,49 +19,45 @@ CONTACT_IDS = ['30551', '10101', '267851', '30122']  # Replace with actual conta
 # Define the API URL pattern
 API_URL_PATTERN = 'https://api.hubapi.com/contacts/v1/contact/vid/{}/profile'
 
-all_contacts_data = {}  # Dictionary to store all contact data
-
-headers = {
-    'Authorization': f'Bearer {API_KEY}'
-}
-
 # Define a default value for missing fields
 DEFAULT_VALUE = "N/A"
 
-try:
-    for contact_id in CONTACT_IDS:
-        # Fetch the contact data using the current contact ID
-        api_url = API_URL_PATTERN.format(contact_id)
-        response = requests.get(api_url, headers=headers)
-        
-        # Check if the response is successful before proceeding
-        if response.status_code == 200:
-            contact_data = response.json()
+def fetch_contact_data(contact_id):
+    api_url = API_URL_PATTERN.format(contact_id)
+    response = requests.get(api_url, headers={'Authorization': f'Bearer {API_KEY}'})
+    if response.status_code == 200:
+        return response.json()
+    return None
 
-            # Provide default values for missing fields
-            for field in ['accreditation', 'firstname', 'lastname', 'email', 'speciality', 'directory___work_phone', 'workaddress1', 'hs_language']:
-                if 'properties' in contact_data and field not in contact_data['properties']:
-                    contact_data['properties'][field] = {'value': DEFAULT_VALUE}
+def provide_default_values(contact_data):
+    for field in ['accreditation', 'firstname', 'lastname', 'email', 'speciality', 'directory___work_phone', 'workaddress1', 'hs_language']:
+        if 'properties' in contact_data and field not in contact_data['properties']:
+            contact_data['properties'][field] = {'value': DEFAULT_VALUE}
 
-            # Store the contact data in the dictionary
-            all_contacts_data[contact_id] = contact_data
+def save_data_as_json(data, filename):
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
 
-    # Save all contact data as a single JSON file
-    with open('single-contact.json', 'w') as json_file:
-        json.dump(all_contacts_data, json_file, indent=2)
-
-    print("All contact data saved to single-contact.json")
-
-    # Render HTML template using Jinja2
-    with open('templates/single-contact-template.html', 'r') as template_file:
+def render_template_and_save(data, template_path, output_path):
+    with open(template_path, 'r') as template_file:
         template_content = template_file.read()
         template = Template(template_content)
-        rendered_html = template.render(contacts=all_contacts_data)
-
-    # Save rendered HTML to a file
-    with open('single-contact.html', 'w') as html_file:
+        rendered_html = template.render(contacts=data)
+    with open(output_path, 'w') as html_file:
         html_file.write(rendered_html)
 
+try:
+    all_contacts_data = {}
+    for contact_id in CONTACT_IDS:
+        contact_data = fetch_contact_data(contact_id)
+        if contact_data:
+            provide_default_values(contact_data)
+            all_contacts_data[contact_id] = contact_data
+
+    save_data_as_json(all_contacts_data, 'single-contact.json')
+    print("All contact data saved to single-contact.json")
+
+    render_template_and_save(all_contacts_data, 'templates/single-contact-template.html', 'single-contact.html')
     print("HTML contact info saved to single-contact.html")
 
 except requests.exceptions.RequestException as e:
